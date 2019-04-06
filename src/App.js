89011152444a5f52756eab20493551e9bb7bd4ab
Anders Holt Knudsen <andhk11@gmail.com>
2019-04-06 13:34:14 +0200

    updated UI

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import SpotifyQueue from './components/SpotifyQueue';
 import SearchBar from './components/SearchBar'
-import './App.css';
+import CurrentlyPlaying from './components/CurrentlyPlaying'
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="header"><h1>The is Spotify! Yeah!</h1></div>
-        <div className="currentlyPlaying"><CurrentlyPlaying /></div>
-        <div className="searchBar"><SearchBar /></div>
+        <div className="playing"><CurrentlyPlaying /></div>
+        <div className="search"><SearchBar /></div>
         <div className="queue"><SpotifyQueue /></div>
         <div className="footer">Here is the footer!</div>
       </div>
---
+++ src/components/CurrentlyPlaying.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class CurrentlyPlaying extends Component {
+  state = {};
+  render() {
+    return <p>Now playing ...</p>;
+  }
+}
+
+export default CurrentlyPlaying;
