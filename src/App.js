56f445e84a55cfffba9480de70803e8e32db832e
Casper Rysgaard <casperrysgaard@gmail.com>
2019-04-06 17:03:31 +0200

    Made app connect to socket server

--- src/App.js
+++ src/App.js
@@ -6,7 +6,26 @@ import SearchBar from "./components/SearchBar";
 import CurrentlyPlaying from "./components/CurrentlyPlaying";
 import "./css/App.css";
 
+import io from 'socket.io-client';
+// const socket = io('192.168.43.86:8000');
+const socket = io('localhost:8000');
+
 class App extends Component {
+  componentDidMount() {
+    socket.on('connect', () => {
+      console.log("Connected:", socket.connected);
+    });
+
+    socket.on('initial setup', (data) => {
+      //this.setState({ dataGot: data })
+    });
+
+    socket.on('kage?', (data) => {
+      console.log(data);
+      //this.setState({ dataGot: [ ... this.state.dataGot, data]})
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
