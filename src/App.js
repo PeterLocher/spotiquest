4007f1c5a5b6479e4b367507d5871dfe89dc31b1
Anders Holt Knudsen <andhk11@gmail.com>
2019-04-06 12:22:43 +0200

    Starting making the UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import SpotifyContainer from './SpotifyContainer';
+import SpotifyQueue from './SpotifyQueue';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>This is Spotify!</h1>
-        <SpotifyContainer />
+        <div className="header"><h1>The is Spotify! Yeah!</h1></div>
+        <div className="searchBar">Here you can search!</div>
+        <div className="queue"><SpotifyQueue /></div>
+        <div className="footer">Here is the footer!</div>
       </div>
     );
   }
---
+++ src/SpotifyQueue.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class SpotifyQueue extends Component {
+  render() {
+    return (
+      <div>
+        <p>Some paragraph</p>
+        <p>Some paragraph</p>
+        <p>Some paragraph</p>
+        <p>Some paragraph</p>
+      </div>
+    );
+  }
+}
+
+export default SpotifyQueue;
