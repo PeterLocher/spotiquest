2c9aa87fec54b6b3d327e63c92ca7a9b10b8fb27
Anders Holt Knudsen <andhk11@gmail.com>
2019-04-07 11:10:11 +0200

    Updated the design!

--- src/components/CustomizedInputBase.jsx
+++ src/components/CustomizedInputBase.jsx
@@ -1,37 +1,44 @@
-import React, { Component } from 'react';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import './CustomizedInputBase.css';
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import "./CustomizedInputBase.css";
 
 class CustomizedInputBase extends Component {
   state = {
-    inputValue: ''
+    inputValue: ""
   };
 
-  handleChangeInput = (e) => {
+  handleChangeInput = e => {
     const value = e.currentTarget.value;
     this.setState({ inputValue: value });
   };
 
   addSong = () => {
     this.props.songSender(this.state.inputValue);
-    this.setState({ inputValue: '' })
+    this.setState({ inputValue: "" });
   };
+
+  enterPressed(event) {
+    const code = event.keyCode || event.which;
+    if (code === 13) {
+      this.addSong()
+    }
+  }
 
   render() {
     return (
       <Paper className="root" elevation={1} square="false">
-        <InputBase className="input"
+        <InputBase
+          className="input"
           placeholder="Search Spotify ..."
           value={this.state.inputValue}
           onChange={this.handleChangeInput}
+          onKeyPress={this.enterPressed.bind(this)}
         />
         <IconButton className="iconButton" aria-label="Search">
-          <SearchIcon
-            onClick={this.addSong}
-          />
+          <SearchIcon onClick={this.addSong} />
         </IconButton>
       </Paper>
     );
